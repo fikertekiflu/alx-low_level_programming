3fb2c728d5671635574780de59694afb8eaf942c
fikertekiflu <casssiopfiker@gmail.com>
2023-09-09 04:28:33 -0700

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar('0' + number);
+	}
+
+	for (number = 0; number < 6; number++)
+	{
+		putchar('a' + number);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
