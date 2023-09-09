2f856fa4ca844aabb7c0e1167545860e5e6d7f35
fikertekiflu <casssiopfiker@gmail.com>
2023-09-09 07:51:16 -0700

    print comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,28 @@
  */
 int main(void)
 {
-	int num1, num2;
+	int num1, num2, num3, num4;
 
-	for (num1 = 0; num1 <= 99; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = num1; num2 <= 99; num2++)
+		for (num2 = 0; num2 <= 9; num2++)
 		{
-			int num1_first_digit = num1 / 10;
-			int num1_second_digit = num1 % 10;
-			int num2_first_digit = num2 / 10;
-			int num2_second_digit = num2 % 10;
-
-			putchar(num1_first_digit + '0');
-			putchar(num1_second_digit + '0');
-			putchar(' ');
-			putchar(num2_first_digit + '0');
-			putchar(num2_second_digit + '0');
-
-			if (num1 != 99 || num2 != 99)
+			for (num3 = num1; num3 <= 9; num3++)
 			{
-				putchar(',');
-				putchar(' ');
+				for (num4 = num2 + 1; num4 <= 9; num4++)
+				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(' ');
+					putchar(num3 + '0');
+					putchar(num4 + '0');
+
+					if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
 	}
