335033a98f2bd01b59011d337eba3c40ad498824
fikertekiflu <casssiopfiker@gmail.com>
2023-09-09 04:11:11 -0700

    print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar('0' + number);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
