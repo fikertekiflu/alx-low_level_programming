dfa630c0ae7c2419a97572f2b5f3d6d5b3855271
fikertekiflu <casssiopfiker@gmail.com>
2023-09-14 08:46:24 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,12 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-		n = -n; /* Convert negative numbers to positive */
+	last_digit = n % 10;
 
-	last_digit = n % 10; /* Get the remainder when divided by 10 */
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
-	_putchar(last_digit + '0'); /* Print the last digit */
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
