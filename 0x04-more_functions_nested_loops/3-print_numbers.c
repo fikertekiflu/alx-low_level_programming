cb379002f8363c8dad01c3ced2a358bcc4d53b29
fikertekiflu <casssiopfiker@gmail.com>
2023-09-15 07:49:37 -0700

    multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+	int i;
+	char digit;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		digit = i + '0';
+		_putchar(digit);
+	}
+	
+	_putchar('\n');
+}
