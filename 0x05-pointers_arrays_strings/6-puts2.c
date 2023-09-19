70f6decaa1a56ef6fe568d94e8aea35f9ba05367
fikertekiflu <casssiopfiker@gmail.com>
2023-09-19 09:11:06 -0700

    string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,9 +6,15 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
+	int j = 0;
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	i = 0;
+	while (i < j)
 	{
 		putchar(str[i]);
 		i += 2;
