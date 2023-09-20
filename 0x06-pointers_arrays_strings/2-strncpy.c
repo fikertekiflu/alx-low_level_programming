0c065cce80e1ba16eb60745f60ba579459189f46
fikertekiflu <casssiopfiker@gmail.com>
2023-09-20 16:00:47 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,12 @@ char *_strncpy(char *dest, char *src, int n)
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	while (i < n)
 	{
 		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
